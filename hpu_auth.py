d36dadc304300fe7e9e85c4a780067fae9b89049
Co2n <co2ncos1t@gmail.com>
2023-05-21 14:28:46 +0700

    Initial Commit

--- hpu_auth.py
+++ hpu_auth.py
@@ -2,7 +2,7 @@ import paramiko
 
 root_uid = "root"
 root_pwd = "rootpassword"
-newpass_mdaces = "P@ssw0rd1"
+newpass_user = "P@ssw0rd1"
 #read file
 file1 = open('list.txt', 'r')
 Lines = file1.readlines()
@@ -20,43 +20,43 @@ try:
          for line in output:
                if line.rstrip() == "export":
                   print("creating user ..... using export directory")
-                  stdin, stdout, stderr = client.exec_command('useradd mdaces -c "User ID Manage PIM"')
+                  stdin, stdout, stderr = client.exec_command('useradd user -c "User ID Manage PIM"')
                   output = stdout.readlines()
                   f.write(output + "\n")
          print("creating user ....... default directory")
          try:
-              (stdin, stdout, stderr) = client.exec_command('sudo useradd mdaces -c "User ID Manage PIM"')
+              (stdin, stdout, stderr) = client.exec_command('sudo useradd user -c "User ID Manage PIM"')
               output1 = stdout.read()
             #   f.write(output1)
-              print("User mdaces created")
+              print("User user created")
          except Exception as e:
               print(e)
             #   f.write(e)
          
-         print("Change password mdaces")
+         print("Change password user")
          try:
-              (stdin, stdout, stderr) = client.exec_command('sudo passwd mdaces')
-              stdin.write(newpass_mdaces + '\n')
-              stdin.write(newpass_mdaces + '\n')
+              (stdin, stdout, stderr) = client.exec_command('sudo passwd user')
+              stdin.write(newpass_user + '\n')
+              stdin.write(newpass_user + '\n')
               stdin.flush()
               output1 = stdout.readlines()
             #   f.write(output1)
-              print("Password mdaces changed")
+              print("Password user changed")
          except Exception as e:
               print(e)
             #   f.write(e)
          
-         print("VISUDO mdaces")
+         print("VISUDO user")
          try:
             stdin, stdout, stderr = client.exec_command("cat /root/test.conf")
             all_lines = ''
             for line in stdout.readlines():
                all_lines += line
             new_line = all_lines + '\n' 
-            (stdin, stdout, stderr) = client.exec_command("echo 'mdaces  ALL=(ALL)   NOPASSWD:ALL' >> /etc/sudoers".format(new_line))
+            (stdin, stdout, stderr) = client.exec_command("echo 'user  ALL=(ALL)   NOPASSWD:ALL' >> /etc/sudoers".format(new_line))
             output1 = stdout.readlines()
             # f.write(output1)
-            print("VISUDO for mdaces changed")
+            print("VISUDO for user changed")
          except Exception as e:
               print(e)
             #   f.write(e)
